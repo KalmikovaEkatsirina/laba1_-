d3ac86dd0935f2930b6a940abb483b210d219e28
Kate <89986771+KalmikovaEkatsirina@users.noreply.github.com>
2022-02-19 21:48:46 +0300

    Add files via upload

---
+++ lab1/calc/calc/Calculator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace calc
+{
+    public static class Calculator
+    {
+
+        public static void operationNE(TextBox textBox)
+        {
+
+            if (textBox.Text == "")
+            {
+                MessageBox.Show("Введите значение");
+            }
+            else
+            {
+                int number;
+                number = Convert.ToInt32(textBox.Text);
+                textBox.Text = Convert.ToString(~number);
+            }
+        }
+
+        public static void inputNumber(KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != 45) // цифры , клавиша BackSpace и минус
+            {
+                e.Handled = true;
+            }
+        }
+
+        public static void clearFields(TextBox tb1, TextBox tb2, TextBox tb3, TextBox tb4, TextBox tb5, TextBox tb6, ComboBox cb)
+        {
+            if (tb1.Text == "" && tb2.Text == "" && tb3.Text == "" && tb4.Text == "" && tb5.Text == "" && tb6.Text == "" && cb.Text == "")
+            {
+                MessageBox.Show("Все поля очищены");
+            }
+            else
+            {
+                tb1.Text = "";
+                tb2.Text = "";
+                tb3.Text = "";
+                tb4.Text = "";
+                tb5.Text = "";
+                tb6.Text = "";
+                cb.Text = "";
+            }
+
+
+        }
+        public static void calculate(TextBox tb1, TextBox tb2, TextBox tb3, TextBox tb4, TextBox tb5, TextBox tb6, ComboBox cb)
+        {
+            if (tb1.Text == "" || tb2.Text == "")
+            {
+                MessageBox.Show("Введите недостающее(ие) значение(я)");
+
+            }
+            else if (cb.Text == "")
+            {
+                MessageBox.Show("Выберите операцию");
+            }
+
+            else
+            {
+                int a, b;
+                a = Convert.ToInt32(tb1.Text);
+                b = Convert.ToInt32(tb1.Text);
+
+                switch (cb.Text)
+                {
+                    case "И":
+                        tb3.Text = Convert.ToString(a & b);
+                        tb4.Text = Convert.ToString(a & b, 2);
+                        tb5.Text = Convert.ToString(a & b, 8);
+                        tb6.Text = Convert.ToString(a & b, 16);
+                        break;
+                    case "ИЛИ":
+                        tb3.Text = Convert.ToString(a | b);
+                        tb4.Text = Convert.ToString(a | b, 2);
+                        tb5.Text = Convert.ToString(a | b, 8);
+                        tb6.Text = Convert.ToString(a | b, 16);
+                        break;
+
+                    case "Исключающее ИЛИ":
+                        tb3.Text = Convert.ToString(a ^ b);
+                        tb4.Text = Convert.ToString(a ^ b, 2);
+                        tb5.Text = Convert.ToString(a ^ b, 8);
+                        tb6.Text = Convert.ToString(a ^ b, 16);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ lab1/calc/calc/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace calc
+{
+    public partial class Form1 : Form
+    {
+
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.KeyPress += textBox1_KeyPress;
+            textBox2.KeyPress += textBox2_KeyPress;
+            button5.Click += button5_Click;
+            button1.Click += button1_Click;
+            button2.Click += button2_Click;
+            button3.Click += button3_Click;
+        }
+
+        
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Calculator.inputNumber(e);          
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            Calculator.inputNumber(e);
+
+        }
+        public  void button5_Click(object sender, EventArgs e)
+        {
+            
+            Calculator.clearFields(textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, comboBox1);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculator.calculate(textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, comboBox1);
+        }
+        
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Calculator.operationNE(textBox1);
+        }  
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Calculator.operationNE(textBox2);
+        }
+
+       
+    }
+}
